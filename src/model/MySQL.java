f9e7c1ba232083d2f315b9f7a85b3d355851dad5
SahanDilusha <sahancodelygroup@gmail.com>
2024-05-06 20:43:27 +0530

    new project update

---
+++ src/model/MySQL.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MySQL {
+
+    private static Connection connection;
+
+    private static final String username = "root";
+    private static final String password = "Sahan@200212010";
+    private static final String database = "shop_db";
+
+    static {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + database, username, password);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ResultSet execute(String query) throws Exception {
+
+        Statement statement = connection.createStatement();
+
+        if (query.startsWith("SELECT")) {
+            return statement.executeQuery(query);
+        } else {
+            statement.executeUpdate(query);
+            return null;
+        }
+
+    }
+
+}
