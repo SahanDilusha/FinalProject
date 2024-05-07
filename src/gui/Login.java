54de368d07b3234904d52bd14c2900c8b8554b09
SahanDilusha <sahancodelygroup@gmail.com>
2024-05-07 09:17:50 +0530

    Login From uapdate

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -233,6 +233,7 @@ public class Login extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         if (jTextField1.getText().isBlank()) {
             
+        }else{
         }
     }//GEN-LAST:event_jButton2ActionPerformed
 
